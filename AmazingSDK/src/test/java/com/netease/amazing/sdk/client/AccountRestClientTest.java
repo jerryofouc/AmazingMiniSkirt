78d993eedc83a9164a987fd028d1b7f84fa3f048
jerryofouc <zhangxiaojiea@163.com>
2013-07-17 22:05:04 +0800

    提交AccountRestClient

--- AmazingSDK/src/test/java/com/netease/amazing/sdk/client/AccountRestClientTest.java
+++ AmazingSDK/src/test/java/com/netease/amazing/sdk/client/AccountRestClientTest.java
@@ -1,14 +1,16 @@
 package com.netease.amazing.sdk.client;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class AccountRestClientTest {
 	private static final String BASE_URL = "http://localhost:8080/MiniSkirtServer";
-	private static final String USER_NAME = "zhangxiaojie";
+	private static final String USER_NAME = "zhanxiaojie";
 	private static final String PASSWORD = "123456";
 	@Test
 	public void loginTest(){
 		//AccountRestClient accountRestClient = new AccountRestClient(BASE_URL,USER_NAME,PASSWORD);
-		AccountRestClient.testLogin(BASE_URL,USER_NAME, PASSWORD);
+		assertTrue(AccountRestClient.testLogin(BASE_URL,USER_NAME, PASSWORD));
 	}
 }
