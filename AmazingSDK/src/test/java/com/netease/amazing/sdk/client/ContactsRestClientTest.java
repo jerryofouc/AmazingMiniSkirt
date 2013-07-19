61c99f8ae2f0cfc496d1357d5ac306b98e16e57c
jerryofouc <zhangxiaojiea@163.com>
2013-07-19 09:39:50 +0800

    更新欧美

--- AmazingSDK/src/test/java/com/netease/amazing/sdk/client/ContactsRestClientTest.java
+++ AmazingSDK/src/test/java/com/netease/amazing/sdk/client/ContactsRestClientTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import com.google.gson.Gson;
 
 public class ContactsRestClientTest {
-	private static final String BASE_URL = "http://localhost:8090/MiniSkirtServer";
+	private static final String BASE_URL = "http://10.240.34.42:8090/MiniSkirtServer";
 	private static final String USER_NAME = "xukai";
 	private static final String PASSWORD = "123456";
 	@Test
