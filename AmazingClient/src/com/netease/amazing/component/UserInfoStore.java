77a727a0b378c60745b67e088b7056c11b5a559a
SYSTEM <SYSTEM@HIH-D-2065>
2013-07-22 11:06:34 +0800

    字符串修正

---
+++ AmazingClient/src/com/netease/amazing/component/UserInfoStore.java
@@ -0,0 +1,9 @@
+package com.netease.amazing.component;
+
+public class UserInfoStore {
+	public static String username ="xukai";
+	public static String password ="123456";
+	public static String serverIpAddress ="10.240.34.42";
+	public static String serverPort ="8080";
+	public static String url = "http://" + serverIpAddress + ":"+serverPort + "/server";
+}
